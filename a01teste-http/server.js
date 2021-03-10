f02cad18dab1f4753fd9af101cf7ac51b8216e2d
Sergio Bonato <asbonato@gmail.com>
2021-03-09 22:38:46 -0300

    melhorias na aula01

--- a01teste-http/server.js
+++ a01teste-http/server.js
@@ -1,88 +1,86 @@
-/*
- * Aula 01 - Protocolo HTTP
- * Código do exemplo mais o código do exercício proposto.
-*/
-
 const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-let contador = 3;
+const express = require('express')
 const app = express();
-const porta = 3000;
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+let contador = 2;
+const porta = 3000;
 app.set('port', porta);
-app.get("/teste", (req, res, next) => {
-    res.send("Olá");
+
+app.get('/teste', (req, res, next) => {
+    res.send("Olá");    
 });
+
 const server = http.createServer(app);
 server.listen(3000);
 
+/*
+C - create (insert) - POST
+R - retrieve (select) - GET
+U - update - PUT
+D - delete - DELETE
+*/
+
 const clientes = [
-    {id: 1,
-     nome: 'João',
-     email: 'joao@email.com'
+    {
+        id: 1,
+        nome: 'João',
+        email: 'joao@email.com'
     },
-    {id: 2,
-     nome: 'Cristina',
-     email: 'cristina@email.com' 
+    {
+        id: 2,
+        nome: 'Cristina',
+        email: 'cristina@email.com'
     }
 ]
-//retrieve
-app.get("/clientes", (req, res, next) => {
+
+// Retrieve
+app.get('/clientes', (req, res, next) => {
     res.json(clientes);
-})
-//create
-app.post("/clientes", (req, res, next) => {
+});
+
+// Create
+app.post('/clientes', (req, res, next) => {
     const cliente = req.body;
-    clientes.push({id: contador += 1, nome: cliente.nome, email: cliente.email});
+    clientes.push({id: contador += 1, nome: cliente.nome, email: cliente.email });
     console.log(clientes);
-    //res.end();
     res.status(201).json(clientes);
-})
-//update
-app.put("/clientes", (req, res, next) => {
+});
+
+// Update
+app.put('/clientes', (req, res, next) => {
+    const novo = req.body;
     clientes.forEach((cliente) => {
-        if(cliente.id === req.body.id){
-            cliente.nome = req.body.nome;
-            cliente.email = req.body.email;             
-        }
-    })
-    res.status(200).json(clientes);
-})
-
-//delete
-app.delete("/clientes", (req, res, next) => {
-    clientes.forEach(cliente => {
-        if (cliente.id === req.body.id) {
-            var index = clientes.indexOf(cliente.id);
-            clientes.splice(index)
+        if(cliente.id === novo.id){
+            cliente.nome = novo.nome;
+            cliente.email = novo.email;
         }
     });
-    res.status(200).json(clientes);
-});
-/*
-//delete passando o id via json no body
-app.delete("/clientes", (req, res, next) => {
-    clientes.forEach(cliente => {
-        if (cliente.id === req.body.id) {
-            var index = clientes.indexOf(cliente.id);
-            clientes.splice(index)
-        }
-    });
-    res.status(200).json(clientes);
-});
-*/
-//outro delete, mas agora passando o id na chamada da requisição
-app.delete('/clientes/:id', (req, res, next) => {
-    const idClienteDeletado = req.params.id;
-    clientes.forEach((cliente, index) => {
-        if(cliente.id == idClienteDeletado) clientes.splice(index)
-    })
-
     console.log(clientes);
     res.status(200).json(clientes);
-})
+});
 
+//Delete
+app.delete('/clientes/:id', (req, res, next) => {
+    const id_apagar = req.params.id;
+    clientes.forEach((cliente, index) => {
+        if(cliente.id == id_apagar){
+            clientes.splice(index, 1);
+        }
+    });
+    console.log(clientes);
+    res.status(200).json(clientes);
+});
 
-
-
+//Retrieve com parâmetro
+app.get('/clientes/:id', (req, res, next) => {
+    const id_consultar = req.params.id;
+    retorno = 'id ' + id_consultar + ' não encontrado'
+    clientes.forEach((cliente) => {
+        if(cliente.id == id_consultar){
+            retorno = cliente;
+        }
+    });
+    console.log(retorno);
+    res.status(200).json(retorno);
+});
